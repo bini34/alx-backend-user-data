fed691238fd38086c7eef8f87fc34c125fb0b80f
bini34 <biniyamambachew@gmail.com>
2024-08-31 20:22:16 +0300

    update tasks

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -29,9 +29,8 @@ def filter_datum(fields: List[str], redaction: str,
 
 
 class RedactingFormatter(logging.Formatter):
-"""
-Redacting Formatter class
-"""
+    """ Redacting Formatter class
+        """
 
     REDACTION = "***"
     FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
